47f21cd1c30825678498ff6b807d62a5933befd7
Tose11 <farjanaaktertose10@gmail.com>
2025-09-10 02:17:07 +0600

    Add files via upload

---
+++ AlphabetPrinter.java
@@ -0,0 +1,16 @@
+
+package labassignment;
+
+public class AlphabetPrinter {
+    public static void main(String[] args){
+        char ch = 'a';
+        
+        while(ch<='z')
+        {
+              System.out.print(ch + " ");
+              ch++;
+        }
+
+    }
+    
+}
